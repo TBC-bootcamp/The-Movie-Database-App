3732023b2ba347f88c289ac3aa9f1bb43305b848
IonaBartishvili <iona.bartishvili.1@btu.edu.ge>
2020-06-03 01:30:12 +0400

    IT'S FUCKING FINAL !! I GUESS...

--- app/src/main/java/com/example/themoviedb/apis/HttpRequests.kt
+++ app/src/main/java/com/example/themoviedb/apis/HttpRequests.kt
@@ -14,10 +14,10 @@ import retrofit2.http.Url
 object HttpRequests {
 
     val retrofit = Retrofit.Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(ScalarsConverterFactory.create())
 //        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl("http://api.themoviedb.org/3/")
-        .build()
+            .baseUrl("http://api.themoviedb.org/3/")
+            .build()
 
     val service = retrofit.create(ApiService::class.java)
 
@@ -42,27 +42,31 @@ object HttpRequests {
 
     fun getMovieDetails(id: Int, callback: CustomCallback) {
         service.getMovieDetails(id.toString(), ApiPaths.API_KEY, "credits")
-            .enqueue(onCallback(callback))
+                .enqueue(onCallback(callback))
     }
 
     fun getTVShowDetails(id: Int, callback: CustomCallback) {
         service.getTvShowDetails(id.toString(), ApiPaths.API_KEY, "credits")
-            .enqueue(onCallback(callback))
+                .enqueue(onCallback(callback))
     }
 
     fun getSearchMovies(queryString: String, callback: CustomCallback) {
         service.searchMovies(ApiPaths.API_KEY, queryString).enqueue(onCallback(callback))
     }
 
-    fun getSimilarMovies(movieID : Int, callback: CustomCallback){
-        service.similarMovies(movieID.toString(), ApiPaths.API_KEY).enqueue(onCallback(callback))
+    fun getSimilarMovies(movieID: Int, callback: CustomCallback) {
+        service.similarMovies("movie", movieID.toString(), ApiPaths.API_KEY).enqueue(onCallback(callback))
     }
 
-    fun getVideos(mediaType: String, movieID: Int, callback: CustomCallback){
+    fun getSimilarTVShows(movieID: Int, callback: CustomCallback) {
+        service.similarMovies("tv", movieID.toString(), ApiPaths.API_KEY).enqueue(onCallback(callback))
+    }
+
+    fun getVideos(mediaType: String, movieID: Int, callback: CustomCallback) {
         service.movieVideos(mediaType, movieID.toString(), ApiPaths.API_KEY).enqueue(onCallback(callback))
     }
 
-    fun getAiringTodayTV(callback: CustomCallback){
+    fun getAiringTodayTV(callback: CustomCallback) {
         service.getAiringTodayTV(ApiPaths.API_KEY).enqueue(onCallback(callback))
     }
 
@@ -82,36 +86,37 @@ object HttpRequests {
 
         @GET("movie/{movie_id}?")
         fun getMovieDetails(
-            @Path("movie_id") id: String,
-            @Query("api_key") apiKey: String,
-            @Query("append_to_response") response: String
+                @Path("movie_id") id: String,
+                @Query("api_key") apiKey: String,
+                @Query("append_to_response") response: String
         ): Call<String>
 
 
         @GET("tv/{tv_id}?")
         fun getTvShowDetails(
-            @Path("tv_id") id: String,
-            @Query("api_key") apiKey: String,
-            @Query("append_to_response") response: String
+                @Path("tv_id") id: String,
+                @Query("api_key") apiKey: String,
+                @Query("append_to_response") response: String
         ): Call<String>
 
         @GET("search/movie?")
         fun searchMovies(
-            @Query("api_key") apiKey: String,
-            @Query("query") queryString: String,
-            @Query("include_adult") adult: String = "false"
+                @Query("api_key") apiKey: String,
+                @Query("query") queryString: String,
+                @Query("include_adult") adult: String = "false"
         ): Call<String>
 
-
-        @GET("movie/{movie_id}/similar?")
+        @GET("{media_type}/{movie_id}/similar?")
         fun similarMovies(
+                @Path("media_type") mediaType: String,
                 @Path("movie_id") id: String,
                 @Query("api_key") apiKey: String
         ): Call<String>
 
+
         @GET("{media_type}/{movie_id}/videos?")
         fun movieVideos(
-                @Path("media_type") mediaType : String,
+                @Path("media_type") mediaType: String,
                 @Path("movie_id") id: String,
                 @Query("api_key") apiKey: String
         ): Call<String>
--- app/src/main/java/com/example/themoviedb/ui/details_page/TvShowDetailsActivity.kt
+++ app/src/main/java/com/example/themoviedb/ui/details_page/TvShowDetailsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.themoviedb.R
@@ -12,9 +13,12 @@ import com.example.themoviedb.apis.HttpRequests
 import com.example.themoviedb.databinding.ActivityTvShowDetailsBinding
 import com.example.themoviedb.models.Movie
 import com.example.themoviedb.models.TVShow
+import com.example.themoviedb.models.TrendingMovies
 import com.example.themoviedb.models.Video
 import com.example.themoviedb.recyclerviews.CreatorsRecyclerViewAdapter
 import com.example.themoviedb.recyclerviews.MovieCastRecyclerAdapter
+import com.example.themoviedb.recyclerviews.TrendingMoviesRecyclerAdapter
+import com.example.themoviedb.recyclerviews.ViewItemClickListener
 import com.example.themoviedb.ui.details_page.viewmodels.TVShowsViewModel
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_tv_show_details.*
@@ -23,27 +27,31 @@ import retrofit2.Response
 class TvShowDetailsActivity : AppCompatActivity() {
     // Creators
     private lateinit var creatorsAdapter: CreatorsRecyclerViewAdapter
+    lateinit var similarTVShowsAdapter: TrendingMoviesRecyclerAdapter
     private var creators = ArrayList<TVShow.Creator>()
 
     // Cast
     private lateinit var castAdapter: MovieCastRecyclerAdapter
+    private var similarTVShows = ArrayList<TrendingMovies.TrendingMoviesList>()
     private var cast = ArrayList<Movie.Cast>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Binding
         val binding: ActivityTvShowDetailsBinding =
-                DataBindingUtil.setContentView(this, R.layout.activity_tv_show_details)
+            DataBindingUtil.setContentView(this, R.layout.activity_tv_show_details)
 
         val tvID = intent.getIntExtra("TVShowID", 0)
 
         // RecyclerView for Creators
-        creatorsRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        creatorsRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         creatorsAdapter = CreatorsRecyclerViewAdapter(creators)
         creatorsRecyclerView.adapter = creatorsAdapter
 
         // RecyclerView for Cast
-        tvShowCastRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        tvShowCastRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         castAdapter = MovieCastRecyclerAdapter(cast)
         tvShowCastRecyclerView.adapter = castAdapter
 
@@ -66,6 +74,26 @@ class TvShowDetailsActivity : AppCompatActivity() {
 
         })
 
+
+        // RecyclerView for Similar TV Shows
+        similarTVShowsRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        similarTVShowsAdapter = TrendingMoviesRecyclerAdapter(similarTVShows, onItemClickListener)
+        similarTVShowsRecyclerView.adapter = similarTVShowsAdapter
+
+        // API Request for Similar TV Shows Recycler View
+        HttpRequests.getSimilarTVShows(tvID, object : CustomCallback {
+            override fun onResponse(body: String, response: Response<String>) {
+                val tvShows = Gson().fromJson(body, TrendingMovies::class.java)
+                tvShows.movieList.forEach {
+                    similarTVShows.add(it)
+                }
+
+                similarTVShowsAdapter.notifyDataSetChanged()
+            }
+
+        })
+
         // Get Trailer and direct to Youtube
         trailerBtn.setOnClickListener {
             HttpRequests.getVideos("tv", tvID, object : CustomCallback {
@@ -73,7 +101,8 @@ class TvShowDetailsActivity : AppCompatActivity() {
                     val video = Gson().fromJson(body, Video::class.java)
 
                     val intentUrl = Intent(Intent.ACTION_VIEW)
-                    intentUrl.data = Uri.parse("https://www.youtube.com/watch?v=${video.results[0].key}")
+                    intentUrl.data =
+                        Uri.parse("https://www.youtube.com/watch?v=${video.results[0].key}")
                     startActivity(intentUrl)
 
                 }
@@ -81,6 +110,23 @@ class TvShowDetailsActivity : AppCompatActivity() {
             })
         }
 
+    }
+
+    private val onItemClickListener: ViewItemClickListener = object : ViewItemClickListener {
+        override fun onClick(
+            itemView: View,
+            position: Int,
+            movie: TrendingMovies.TrendingMoviesList
+        ) {
+            itemView.setOnClickListener {
+
+                startActivity(
+                    Intent(applicationContext, TvShowDetailsActivity::class.java)
+                        .putExtra("TVShowID", movie.id)
+                )
+
+            }
+        }
 
     }
 }
